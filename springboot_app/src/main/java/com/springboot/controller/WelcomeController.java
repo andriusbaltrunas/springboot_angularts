52e9b901792dc19d8a95b8126518eb6a78b44d87
andbal <andrius.baltrunas@nomagic.com>
2020-01-31 10:44:10 +0200

    init commit

---
+++ springboot_app/src/main/java/com/springboot/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController
+{
+	@GetMapping("/{name}")
+	public String welcomeController(@PathVariable("name") String name)
+	{
+		return "Welcome " + name;
+	}
+}
